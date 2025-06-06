b4f75302f0fb0a7d4dd6dda777347e93787db874
Philippe Clérié <pclerie@logisys.ht>
2025-06-06 15:18:28 -0400

    feat: Update to latest version of Unifi controller

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 
-$SNAP/usr/lib/jvm/java-8-openjdk-amd64/bin/java \
-  -Dfile.encoding=UTF-8 -Djava.awt.headless=true \
-  -Dapple.awt.UIElement=true -Dunifi.core.enabled=false \
-  -Xmx1024M -XX:+UseParallelGC -XX:+ExitOnOutOfMemoryError \
-  -XX:+CrashOnOutOfMemoryError -XX:ErrorFile=$SNAP_DATA/logs/hs_err_pid%p.log \
+${SNAP}/usr/lib/jvm/java-17-openjdk-${SNAP_ARCH}/bin/java \
+  -Dfile.encoding=UTF-8 \
+  -Djava.awt.headless=true \
+  -Dapple.awt.UIElement=true \
+  -Dunifi.core.enabled=false \
+  -Xmx1024M \
+  -XX:+UseParallelGC \
+  -XX:+ExitOnOutOfMemoryError \
+  -XX:+CrashOnOutOfMemoryError \
+  -XX:ErrorFile=/usr/lib/unifi/logs/unifi_crash.log \
+  -Xlog:gc:logs/gc.log:time:filecount=2,filesize=5M \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
+  --add-opens java.base/java.time=ALL-UNNAMED \
+  --add-opens java.base/java.io=ALL-UNNAMED \
+  --add-opens java.base/sun.security.util=ALL-UNNAMED \
+  --add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED \
   -jar /usr/lib/unifi/lib/ace.jar start
